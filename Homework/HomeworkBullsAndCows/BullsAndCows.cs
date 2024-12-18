4f504228f7488fb7205f252eb6fb643d65b44454
FedorPopenko <popenko.fedor@gmail.com>
2024-12-18 19:55:41 +0400

    fix: goto in do, adding arrays

--- Homework/HomeworkBullsAndCows/BullsAndCows.cs
+++ Homework/HomeworkBullsAndCows/BullsAndCows.cs
@@ -13,10 +13,10 @@
         {
             public PlayerType[] Players { get; set; }
             public PlayerType Winner { get; set; }
-            public int HumanNumber { get; set; }
-            public int HumanAnswer { get; set; }
-            public int MachinNumber { get; set; }
-            public int MachinAnswer { get; set; }
+            public int[] HumanNumber { get; set; }
+            public int[] HumanAnswer { get; set; }
+            public int[] MachinNumber { get; set; }
+            public int[] MachinAnswer { get; set; }
         }
         public GameState State { get; private set; }
 
@@ -52,7 +52,8 @@
 
         private void CalculateWinner(PlayerType player)
         {
-            if (State.HumanNumber == State.MachinAnswer || State.MachinNumber == State.HumanAnswer)
+            if (State.MachinAnswer != null && State.HumanNumber.SequenceEqual(State.MachinAnswer)
+                || State.HumanAnswer != null && State.MachinNumber.SequenceEqual(State.HumanAnswer))
             {
                 State.Winner = player;
                 switch (State.Winner)
@@ -76,7 +77,20 @@
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Random random = new Random();
-            int machinAnswer = random.Next(1000, 9999);
+            int[] oldMachinAnswer = State.MachinAnswer;
+            int[] machinAnswer = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                if (oldMachinAnswer != null && oldMachinAnswer.Contains(State.HumanNumber[i]))
+                {
+                    if (oldMachinAnswer[i] == State.HumanNumber[i])
+                        machinAnswer[i] = oldMachinAnswer[i];
+                    else
+                        machinAnswer[i] = random.Next(oldMachinAnswer.Length);
+                }
+                else
+                    machinAnswer[i] = random.Next(0, 9);
+            }
             State.MachinAnswer = machinAnswer;
             CheckingNumbers(PlayerType.Machin);
             Console.ResetColor();
@@ -86,7 +100,10 @@
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Write a four digit number:");
-            int humanAnswer = int.Parse(Console.ReadLine());
+            int[] humanAnswer = new int[4];
+            //for (int i = 0; i < 4; i++)
+            //    humanAnswer[i] = int.Parse(Console.ReadLine());
+            humanAnswer = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
             State.HumanAnswer = humanAnswer;
             CheckingNumbers(PlayerType.Human);
             Console.ResetColor();
@@ -94,8 +111,8 @@
 
         void CheckingNumbers(PlayerType player)
         {
-            int answer;
-            int number;
+            int[] answer;
+            int[] number;
             if (PlayerType.Human == player)
             {
                 answer = State.HumanAnswer;
@@ -106,15 +123,9 @@
                 answer = State.MachinAnswer;
                 number = State.HumanNumber;
             }
-            int ans1 = answer / 1000 % 10;
-            int ans2 = answer / 100 % 10;
-            int ans3 = answer / 10 % 10;
-            int ans4 = answer % 10;
-            int num1 = number / 1000 % 10;
-            int num2 = number / 100 % 10;
-            int num3 = number / 10 % 10;
-            int num4 = number % 10;
-            if (ans1 == ans2 || ans2 == ans3 || ans3 == ans4 || ans1 == ans3 || ans1 == ans4 || ans2 == ans4)
+            if (answer[0] == answer[1] || answer[1] == answer[2]
+                || answer[2] == answer[3] || answer[0] == answer[2]
+                || answer[0] == answer[3] || answer[1] == answer[3])
             {
                 if (PlayerType.Human == player)
                 {
@@ -130,21 +141,19 @@
             {
                 int countB = 0;
                 int countC = 0;
-                int[] ans = { ans1, ans2, ans3, ans4 };
-                int[] num = { num1, num2, num3, num4 };
                 for (int i = 0; i < 4; i++)
                 {
-                    if (ans[i] == num[i])
+                    if (answer[i] == number[i])
                     {
                         countB++;
                     }
-                    else if (ans.Contains(num[i]))
+                    else if (answer.Contains(number[i]))
                     {
                         countC++;
                     }
                 }
                 if (PlayerType.Machin == player)
-                    Console.WriteLine($"My turn:\n{answer}");
+                    Console.WriteLine($"My turn:\n{answer[0]} {answer[1]} {answer[2]} {answer[3]}");
 
                 Console.WriteLine($"{countB}Bulls,{countC}Cows");
 
@@ -153,36 +162,43 @@
         }
         public void InputValidation()
         {
-        M1: Console.ForegroundColor = ConsoleColor.Red;
-            int humanNumber = int.Parse(Console.ReadLine());
-            Console.ResetColor();
-            int hNum1 = humanNumber / 1000 % 10;
-            int hNum2 = humanNumber / 100 % 10;
-            int hNum3 = humanNumber / 10 % 10;
-            int hNum4 = humanNumber % 10;
-            if (hNum1 == hNum2 || hNum2 == hNum3 || hNum3 == hNum4 || hNum1 == hNum3 || hNum1 == hNum4 || hNum2 == hNum4)
+            int[] humanNumber = new int[4];
+            do
             {
-                Console.WriteLine("You need to enter different numbers!");
-                goto M1;
+                Console.ForegroundColor = ConsoleColor.Red;
+                //for (int i = 0; i < 4; i++)
+                //    humanNumber[i] = int.Parse(Console.ReadLine());
+                humanNumber = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+                Console.ResetColor();
+                if (humanNumber[0] == humanNumber[1] || humanNumber[1] == humanNumber[2]
+                    || humanNumber[2] == humanNumber[3] || humanNumber[0] == humanNumber[2]
+                    || humanNumber[0] == humanNumber[3] || humanNumber[1] == humanNumber[3])
+                {
+                    Console.WriteLine("You need to enter different numbers!");
+                }
+                else
+                {
+                    State.HumanNumber = humanNumber;
+                }
             }
-            else
+            while (State.HumanNumber != humanNumber);
+            int[] machinNumber = new int[4];
+            do
             {
-                State.HumanNumber = humanNumber;
+                Random random = new Random();
+                for (int i = 0; i < 4; i++)
+                    machinNumber[i] = random.Next(0, 9);
+                if (machinNumber[0] == machinNumber[1] || machinNumber[1] == machinNumber[2]
+                    || machinNumber[2] == machinNumber[3] || machinNumber[0] == machinNumber[2]
+                    || machinNumber[0] == machinNumber[3] || machinNumber[1] == machinNumber[3])
+                {
+                }
+                else
+                {
+                    State.MachinNumber = machinNumber;
+                }
             }
-        M2: Random random = new Random();
-            int machinNumber = random.Next(1000, 9999);
-            int mNum1 = machinNumber / 1000 % 10;
-            int mNum2 = machinNumber / 100 % 10;
-            int mNum3 = machinNumber / 10 % 10;
-            int mNum4 = machinNumber % 10;
-            if (hNum1 == hNum2 || hNum2 == hNum3 || hNum3 == hNum4 || hNum1 == hNum3 || hNum1 == hNum4 || hNum2 == hNum4)
-            {
-                goto M2;
-            }
-            else
-            {
-                State.MachinNumber = machinNumber;
-            }
+            while (State.MachinNumber != machinNumber);
         }
     }
 }
